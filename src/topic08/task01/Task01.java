30bc43abe37cd75a32ad7f6aeee5b23ae33e866e
tryvel <tryvel@yandex.com>
2025-05-09 21:53:53 +0300

    Solution of tasks on topic 8

---
+++ src/topic08/task01/Task01.java
@@ -0,0 +1,29 @@
+package topic08.task01;
+
+import java.util.Scanner;
+
+public class Task01 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] numbers = new int[3];
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.print("Введите значение " + (i + 1) + " элемента массива: ");
+            numbers[i] = scanner.nextInt();
+        }
+        System.out.print("Введите делимое: ");
+        int dividend = scanner.nextInt();
+
+        int i = 0;
+        while (true) {
+            try {
+                System.out.println(dividend + " / " + numbers[i] + " = " + (dividend / numbers[i]));
+            } catch (ArithmeticException e) {
+                System.out.println("Деление на ноль запрещено");
+            } catch (ArrayIndexOutOfBoundsException e) {
+                System.out.println("Программа успешно завершена");
+                break;
+            }
+            i++;
+        }
+    }
+}
